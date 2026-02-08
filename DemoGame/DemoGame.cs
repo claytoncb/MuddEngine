b2f2074796d03d0e955631e8b2f37ada6f41c43b
Clayton Bruce <claytoncbruce@gmail.com>
2026-02-08 12:22:16 -0800

    More train sprites, adding floor.

--- DemoGame/DemoGame.cs
+++ DemoGame/DemoGame.cs
@@ -15,26 +15,40 @@ namespace MuddEngine.DemoGame
             
             for (int i = 0; i < 17; i++)
                 {
-                    _ = new Sprite($"Tile{i}", new Vector3(-256 + 32 * i, -32, 0), 0)
+                    _ = new Sprite($"FloorTop{i}", new Vector3(-256 + 32 * i, 32, 0), 0)
                     {
-                        SheetLocation = new (i==0?64:(i==16?32:0), 576),
+                        SheetLocation = new (0, 576),
+                        VisibleSize = new(32, 16),
+                        VisibleOffset = new(0,8),
+                        isFlat = true
+                    };
+                    _ = new Sprite($"FloorLower{i}", new Vector3(-256 + 32 * i, -64, 0), 0)
+                    {
+                        SheetLocation = new (0, 576),
+                        VisibleSize = new(32, 16),
+                        VisibleOffset = new(0,8),
+                        isFlat = true
+                    };
+                    _ = new Sprite($"Carpet{i}", new Vector3(-256 + 32 * i, -16, 0), 0)
+                    {
+                        SheetLocation = new (i==0?64:(i==16?32:0), 608),
                         VisibleSize = new(32, 32),
                         isFlat = true
                     };
-                    _ = new Sprite($"WallBottom{i}", new Vector3(-256 + 32 * i, 32, 0), 0)
+                    _ = new Sprite($"WallBottom{i}", new Vector3(-256 + 32 * i, 80, 0), 0)
                     {
-                        SheetLocation = new (32*(i%4==0?1:(i%4==1 || i%4==3)?2:0)*2, 608),
+                        SheetLocation = new (32*(i%4==0?1:(i%4==1 || i%4==3)?2:0)*2, 640),
                         VisibleSize = new(32, 32),
                     };
-                    _ = new Sprite($"WallTop{i}", new Vector3(-256 + 32 * i, 32, 32), 0)
+                    _ = new Sprite($"WallTop{i}", new Vector3(-256 + 32 * i, 80, 32), 0)
                     {
-                        SheetLocation = new (32*(i%4==0?1:(i%4==1 || i%4==3)?2:0)*2+32, 608),
+                        SheetLocation = new (32*(i%4==0?1:(i%4==1 || i%4==3)?2:0)*2+32, 640),
                         VisibleSize = new(32, 32),
                     };
                     if (i%4==1 || i%4==3)
                     {
-                        _ = new LightSource(new Vector3(-256 + 15.5f + 32 * i, 31, 38.5f), 72f, 1.0f, new Raylib_cs.Color(255, 200, 100));
-                        _ = new LightSource(new Vector3(-256 + 15.5f + 32 * i, -31, 38.5f), 72f, 1.0f, new Raylib_cs.Color(255, 200, 100));
+                        _ = new LightSource(new Vector3(-256 + 15.5f + 32 * i, 58, 38.5f), 108f, 1.0f, new Raylib_cs.Color(255, 200, 100));
+                        _ = new LightSource(new Vector3(-256 + 15.5f + 32 * i, -58, 38.5f), 108f, 1.0f, new Raylib_cs.Color(255, 200, 100));
                     }
                 }
         }
